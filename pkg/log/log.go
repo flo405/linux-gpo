fcce7978e6fe3b3f247f5fe8c9baf1fba2831028
flo405 <80383285+flo405@users.noreply.github.com>
2025-09-14 21:28:11 +0200

    Update log.go

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,10 +1,10 @@
 package log
 
 import (
-    "encoding/json"
-    "fmt"
-    "os"
-    "time"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
 )
 
 type Logger struct{}
@@ -12,14 +12,14 @@ type Logger struct{}
 func New() *Logger { return &Logger{} }
 
 func (l *Logger) log(level, msg string, kv ...string) {
-    m := map[string]any{"ts": time.Now().UTC().Format(time.RFC3339), "level": level, "msg": msg}
-    for i := 0; i+1 < len(kv); i += 2 {
-        m[kv[i]] = kv[i+1]
-    }
-    b, _ := json.Marshal(m)
-    fmt.Fprintln(os.Stdout, string(b))
+	m := map[string]any{"ts": time.Now().UTC().Format(time.RFC3339), "level": level, "msg": msg}
+	for i := 0; i+1 < len(kv); i += 2 {
+		m[kv[i]] = kv[i+1]
+	}
+	b, _ := json.Marshal(m)
+	fmt.Fprintln(os.Stdout, string(b))
 }
-func (l *Logger) Info(msg string, kv ...string) { l.log("info", msg, kv...) }
-defWarn := "warn"
-func (l *Logger) Warn(msg string, kv ...string) { l.log(defWarn, msg, kv...) }
+
+func (l *Logger) Info(msg string, kv ...string)  { l.log("info", msg, kv...) }
+func (l *Logger) Warn(msg string, kv ...string)  { l.log("warn", msg, kv...) }
 func (l *Logger) Error(msg string, kv ...string) { l.log("error", msg, kv...) }
